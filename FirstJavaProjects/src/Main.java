b53c0664a0e2413d399899c2ade0500cbe658b6c
aleoluwatobemmanuel <118288543+aleoluwatobemmanuel@users.noreply.github.com>
2022-11-29 18:19:31 +0100

    firstjavaprojectfortask14

---
+++ FirstJavaProjects/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Good World!");
+
+        System.out.println("Hello africa!");
+        System.out.println("Hello Nigeria!");
+        System.out.println("Great one!");
+        System.out.println("Will you!");
+        System.out.println("Good bye!");
+        System.out.println("Tell me!");
+    }
+}
